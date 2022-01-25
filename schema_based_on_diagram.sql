5375161908e07d6e30cee7758ff71b7f23a93711
gracekabaghe <gracema2002@gmail.com>
2022-01-25 16:54:23 +0200

    Create tables:invoices, invoice_items, medical histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -16,5 +16,38 @@ CREATE TABLE IF NOT EXISTS treatments
     name VARCHAR(100)
 )
 
+--Table: Medical Histories
+DROP TABLE IF EXISTS medical_histories;
+CREATE TABLE IF NOT EXISTS invoices
+(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    admitted_at TIMESTAMP,
+    patient_id INT,
+    status VARCHAR(100)
+)
+
+--Table: Invoices
+DROP TABLE IF EXISTS invoices;
+CREATE TABLE IF NOT EXISTS invoices
+(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    total_amount DECIMAL,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id INT
+)
+
+--Table: Invoice_items
+DROP TABLE IF EXISTS invoice_items;
+CREATE TABLE IF NOT EXISTS invoice_items
+(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    unit_price DECIMAL,
+    quantity INT,
+    total_price DECIMAL,
+    invoice_id INT,
+    treatment_id INT
+)
+
 
 
